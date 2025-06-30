d031305972e54b1d9f3b7c4c034956712ca0043f
Diao <ldiao@lapogpuap10.ad.medctr.ucla.edu>
2025-06-30 16:20:33 -0700

    “update”

---
+++ ld_scripts/debug.py
@@ -0,0 +1,10 @@
+			'HGBA1C' -- A1C
+			, 'LABINSU' -- Fasting Insulin
+			, 'LABPROI' --
+			, 'TRIGLY'
+			, 'HDL'
+			, 'CHOLHDL'
+			, 'GFR', 'GFREST', 'EGFR'
+			, 'CREAT'
+			, 'RBC', 'WBC', 'PLT' -- CBC
+			, 'K', 'NA' -- BMP
